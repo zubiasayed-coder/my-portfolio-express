ca12860d46b6c5dcab833121f353199a885de8b8
SAYED ZUBIA ANWAR HUSSAIN <zubiasayed@eng.rizvi.edu.in>
2026-04-21 04:10:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req,res) => {
-    res.sendFile(__dirname+"/pages/index.html");
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html");
 });
 
-app.get("/skills", (req,res) => {
-    res.sendFile(__dirname+"/pages/my_skills.html");
+app.get("/skills", (req, res) => {
+    res.sendFile(__dirname + "/pages/my_skills.html");
 });
 
-
-app.listen(port, (err) => {
-    console.log("Server started on http://localhost:"+port);
-})
+app.listen(port, "0.0.0.0", (err) => {
+    if (err) {
+        console.log("Error starting server:", err);
+    } else {
+        console.log("Server started on port " + port);
+    }
+});
